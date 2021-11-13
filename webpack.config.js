861328c5626342f0a3f5960ebc219fb795d685a7
rlovece <rlovece@hotmail.co.uk>
2021-11-13 19:24:45 +0000

    changed icon to png

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = function (_env, argv) {
 		plugins: [
 			new HtmlWebpackPlugin({
 				template: './index.html',
-				favicon: './src/utility/icon/favicon.svg',
+				favicon: './src/utility/icon/favicon.png',
 				inject: true,
 				chunks: ['index'],
 				filename: 'index.html'
